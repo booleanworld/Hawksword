f1aeb2835e483c8511bb2eb3d90a5997195ba512
Maunik Shah <maunik318@gmail.com>
2012-04-05 19:32:22 +0530

    Log Message at Surface is Changed.
    by default there should be a log message displayed in the Logcat
    when the surface of the screen changed.
    
    Signed-off-by: Maunik Shah <maunik318@gmail.com>

--- src/com/bw/hawksword/ocr/CaptureActivity.java
+++ src/com/bw/hawksword/ocr/CaptureActivity.java
@@ -642,6 +642,7 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
   }
 
   public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+	  Log.d(TAG,"SurfaceChanged");
 	   /* if (handler != null) {
 	        handler.quitSynchronously();
 	      }
